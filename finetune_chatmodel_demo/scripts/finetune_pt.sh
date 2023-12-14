0ee065318ce874b1e8e99d2f31bfbcfc4525aa12
Hilda Lynch <826706573@qq.com>
2023-12-14 09:52:00 +0800

    Update finetune_pt.sh

--- finetune_chatmodel_demo/scripts/finetune_pt.sh
+++ finetune_chatmodel_demo/scripts/finetune_pt.sh
@@ -16,8 +16,8 @@ DATESTR=`date +%Y%m%d-%H%M%S`
 RUN_NAME=advertise_gen_pt
 
 BASE_MODEL_PATH=THUDM/chatglm3-6b
-DATASET_PATH=formatted_data/advertise_gen.jsonl
-OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}-${PRE_SEQ_LEN}-${LR}
+DATASET_PATH=/content/formatted_data/advertise_gen.jsonl
+OUTPUT_DIR=/content/output/${RUN_NAME}-${DATESTR}-${PRE_SEQ_LEN}-${LR}
 
 mkdir -p $OUTPUT_DIR
 
